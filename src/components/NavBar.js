22a7f517bbe0fa540a65da9176f1ee55f8afded2
Archit Jain <77613361+architjain798@users.noreply.github.com>
2022-06-27 13:12:25 +0530

    Fixed the styling issue in Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div style={{ display: "flex", alignItems: "space-between" }}>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between"
+      }}
+    >
       <span className="logo">REDUX STORE</span>
       <div>
         <Link className="navLink" to="/">
